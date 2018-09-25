d612e0a13fa3546667a5dcbd3d3dc18c9c0a0a9b
tnordahl <thomas@memento-art.com>
2018-09-25 19:15:40 +0200

    created SpeachBubble React Component and css

--- src/client/components/PirateTextBox/index.js
+++ src/client/components/PirateTextBox/index.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import { translate } from 'pirate-speak';
+import SpeachBubble from './../SpeachBubble';
 
 class PirateTextBox extends PureComponent {
   render() {
     return (
       <div className='pirate-text-box'>
-
+        <SpeachBubble />
+        <SpeachBubble />
       </div>
     );
   }
---
+++ src/client/components/SpeachBubble/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class SpeachBubble extends PureComponent {
+  render() {
+    return (
+      <div className='SpeachBubble'>
+        this is a comment
+      </div>
+    );
+  }
+}
+
+export default SpeachBubble;
